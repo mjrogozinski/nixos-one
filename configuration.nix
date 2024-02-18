6e54924e953e16308b2c7ab4dfc0f3053ed57f8c
mjrogozinski <mjrogozinski@gmail.com>
2024-02-18 22:26:39 +0100

    sudo without password + zsh

--- configuration.nix
+++ configuration.nix
@@ -9,9 +9,27 @@
     packages = with pkgs; [
       cowsay
       lolcat
+      home-manager
+      neovim
+      zsh
+      oh-my-zsh
     ];
     initialPassword = "michal";
+    shell = pkgs.zsh;
   };
 
+  programs.zsh.enable = true;
   system.stateVersion = "23.11";
+  services.getty.autologinUser = "michal";
+
+  # sudo without password
+  security.sudo.extraRules= [
+    {  users = [ "michal" ];
+      commands = [
+         { command = "ALL" ;
+           options= [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
-#
-#
+
 QEMU_KERNEL_PARAMS=console=ttyS0 ./result/bin/run-nixos-vm -nographic; reset
